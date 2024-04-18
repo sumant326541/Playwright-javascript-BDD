b3bb3e4823b90ac3264e1b9814560179a8dc9585
Sumant Kumar <sumantkumar@airasia.com>
2024-04-19 01:58:18 +0530

    refactor locator

--- page-objects/login-page.js
+++ page-objects/login-page.js
@@ -1,4 +1,10 @@
 class LoginPage {
+ constructor(){
+  this.usernameInput = '#user-name'
+  this.passwordInput = '#password'
+  this.loginButton = '#login-button'
+  this.inventoryList = '.inventory_list'
+ }
   async navigateToLoginScreen() {
     // Go to the application
     await page.goto('https://www.saucedemo.com/')
@@ -6,21 +12,21 @@ class LoginPage {
 
   async submitLoginForm() {
     // Login with valid credentials
-    await page.fill('#user-name', 'standard_user')
-    await page.fill('#password', 'secret_sauce')
-    await page.click('#login-button')
+    await page.fill(this.usernameInput, 'standard_user')
+    await page.fill(this.passwordInput, 'secret_sauce')
+    await page.click(this.loginButton)
   }
 
   async submitLoginWithParameters(username, password) {
     // Login with given credentials (Username and Password are coming from Feature file)
-    await page.fill('#user-name', username)
-    await page.fill('#password', password)
-    await page.click('#login-button')
+    await page.fill(this.usernameInput, username)
+    await page.fill(this.passwordInput, password)
+    await page.click(this.loginButton)
   }
 
   async assertUserIsLoggedIn() {
     // Assertion for an web element present on the Homepage (User comes at HomePage after successful login)
-    await page.waitForSelector('.inventory_list')
+    await page.waitForSelector(this.inventoryList)
   }
 
   async pause() {
